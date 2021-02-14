8025a6c6ef041a148a30273bd5b9d87ce24da71b
Marina Anisimova <anisimova.marina.n@gmail.com>
2021-02-14 14:19:18 +0300

    feature: кастомизировать ошибки celebrate

--- routes/movies.js
+++ routes/movies.js
@@ -1,26 +1,89 @@
 const router = require('express').Router();
 const { celebrate, Joi } = require('celebrate');
 const { getMovies, createMovie, deleteMovie } = require('../controllers/movies');
+const errorMessages = require('../utils/constants');
 
 router.get('/movies', getMovies);
 router.post('/movies', celebrate({
   body: Joi.object().keys({
-    country: Joi.string().required(),
-    director: Joi.string().required(),
-    duration: Joi.number().required(),
-    year: Joi.string().required(),
-    description: Joi.string().required(),
-    image: Joi.string().required().regex(/^https?:\/\/(www.)?[\w-]{1,63}\.[\w-]{1,256}[a-z-._~:\/?#[\]@!$&'()*+,;=\w]*#?/i),
-    trailer: Joi.string().required().regex(/^https?:\/\/(www.)?[\w-]{1,63}\.[\w-]{1,256}[a-z-._~:\/?#[\]@!$&'()*+,;=\w]*#?/i),
-    thumbnail: Joi.string().required().regex(/^https?:\/\/(www.)?[\w-]{1,63}\.[\w-]{1,256}[a-z-._~:\/?#[\]@!$&'()*+,;=\w]*#?/i),
-    movieId: Joi.number().required(),
-    nameRU: Joi.string().required().regex(/^[а-яё0-9]+[а-я0-9\s.?!,:;\-=+&'()*]*/i),
-    nameEN: Joi.string().required().regex(/^[a-z0-9]+[a-z0-9\s.?!,:;\-=+&'()*]*/i),
+    country: Joi.string().required()
+      .messages({
+        'string.base': `Название страны ${errorMessages.dataFormat}`,
+        'string.empty': errorMessages.requiredField,
+        'any.required': errorMessages.requiredField,
+      }),
+    director: Joi.string().required()
+      .messages({
+        'string.base': `Имя режиссёра ${errorMessages.dataFormat}`,
+        'string.empty': errorMessages.requiredField,
+        'any.required': errorMessages.requiredField,
+      }),
+    duration: Joi.number().required()
+      .messages({
+        'number.base': `Продолжительность ${errorMessages.dataFormat}`,
+        'any.required': errorMessages.requiredField,
+      }),
+    year: Joi.string().required()
+      .messages({
+        'string.base': `Год ${errorMessages.dataFormat}`,
+        'string.empty': errorMessages.requiredField,
+        'any.required': errorMessages.requiredField,
+      }),
+    description: Joi.string().required()
+      .messages({
+        'string.base': `Описание ${errorMessages.dataFormat}`,
+        'string.empty': errorMessages.requiredField,
+        'any.required': errorMessages.requiredField,
+      }),
+    image: Joi.string().required().regex(/^https?:\/\/(www.)?[\w-]{1,63}\.[\w-]{1,256}[a-z-._~:\/?#[\]@!$&'()*+,;=\w]*#?/i)
+      .messages({
+        'string.base': `Ссылка ${errorMessages.dataFormat}`,
+        'string.empty': errorMessages.requiredField,
+        'any.required': errorMessages.requiredField,
+        'string.pattern.base': `Ссылка ${errorMessages.dataFormat}`,
+      }),
+    trailer: Joi.string().required().regex(/^https?:\/\/(www.)?[\w-]{1,63}\.[\w-]{1,256}[a-z-._~:\/?#[\]@!$&'()*+,;=\w]*#?/i)
+      .messages({
+        'string.base': `Ссылка ${errorMessages.dataFormat}`,
+        'string.empty': errorMessages.requiredField,
+        'any.required': errorMessages.requiredField,
+        'string.pattern.base': `Ссылка ${errorMessages.dataFormat}`,
+      }),
+    thumbnail: Joi.string().required().regex(/^https?:\/\/(www.)?[\w-]{1,63}\.[\w-]{1,256}[a-z-._~:\/?#[\]@!$&'()*+,;=\w]*#?/i)
+      .messages({
+        'string.base': `Ссылка ${errorMessages.dataFormat}`,
+        'string.empty': errorMessages.requiredField,
+        'any.required': errorMessages.requiredField,
+        'string.pattern.base': `Ссылка ${errorMessages.dataFormat}`,
+      }),
+    movieId: Joi.number().required()
+      .messages({
+        'number.base': `Id ${errorMessages.dataFormat}`,
+        'any.required': errorMessages.requiredField,
+      }),
+    nameRU: Joi.string().required().regex(/^[а-яё0-9]+[а-я0-9\s.?!,:;\-=+&'()*]*/i)
+      .messages({
+        'string.base': `Название ${errorMessages.dataFormat}`,
+        'string.empty': errorMessages.requiredField,
+        'any.required': errorMessages.requiredField,
+        'string.pattern.base': `Название ${errorMessages.dataFormat}`,
+      }),
+    nameEN: Joi.string().required().regex(/^[a-z0-9]+[a-z0-9\s.?!,:;\-=+&'()*]*/i)
+      .messages({
+        'string.base': `Название ${errorMessages.dataFormat}`,
+        'string.empty': errorMessages.requiredField,
+        'any.required': errorMessages.requiredField,
+        'string.pattern.base': `Название ${errorMessages.dataFormat}`,
+      }),
   }),
 }), createMovie);
 router.delete('/movies/:movieId', celebrate({
   params: Joi.object().keys({
-    movieId: Joi.string().alphanum().length(24),
+    movieId: Joi.string().alphanum().length(24)
+      .messages({
+        'string.alphanum': `Id ${errorMessages.dataFormat}`,
+        'string.length': `Id ${errorMessages.dataFormat}`,
+      }),
   }),
 }), deleteMovie);
 
--- utils/constants.js
+++ utils/constants.js
@@ -4,6 +4,10 @@ const noMovie = 'Фильма с таким id не существует';
 const noPage = 'Такой страницы не существует';
 const invalidId = 'Невалидный id';
 const invalidData = 'Введённые данные невалидны';
+const dataFormat = 'не соответствует формату';
+const requiredField = 'Поле обязательно';
+const min = 'Минимальное количество символов должно быть';
+const max = 'Максимальное количество символов должно быть';
 const userExists = 'Пользователь уже зарегистрирован';
 const authNeeded = 'Необходима авторизация';
 const othersData = 'Нельзя удалять карточки других пользователей';
@@ -23,4 +27,8 @@ module.exports = {
   movieDeleted,
   serverError,
   noPage,
+  min,
+  max,
+  dataFormat,
+  requiredField,
 };
